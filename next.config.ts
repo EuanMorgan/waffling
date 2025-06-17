189f0b259637c63673dafa19b5a5f4a3750c4481
Euan Morgan <euanmorgan48@gmail.com>
2025-06-17 22:59:29 +0100

    chore: setup ci, biome, renovate etc

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
+// biome-ignore lint/correctness/noUnusedImports: we need to validate the env at build time
+import { env as _validateEnvAtBuildTime } from "./src/env";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+	// We typecheck and lint in CI so don't want these to stop builds succeeding
+	typescript: {
+		ignoreBuildErrors: true,
+	},
+	eslint: {
+		ignoreDuringBuilds: true,
+	},
+	output: "standalone",
+	transpilePackages: ["@t3-oss/env-nextjs", "@t3-oss/env-core"],
 };
 
 export default nextConfig;
